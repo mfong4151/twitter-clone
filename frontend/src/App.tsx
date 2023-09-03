2e2af2448cb306373bc522de36fb37949e8d424c
Max <mfong415@gmail.com>
2023-09-03 15:43:01 -0700

    Added tailwind frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Welcome to Tailwind CSS!</h1>
+      <p className="mt-2">Start building amazing interfaces with ease.</p>
+      <button className="mt-4 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded">Get Started</button>
+    </div>
+  );
+}
+
+export default App;
